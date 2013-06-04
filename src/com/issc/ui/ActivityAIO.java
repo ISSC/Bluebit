3977d382304d90bdb250829a0d6184caed35c650
Julian_Chu <julian_chu@issc-tech.com>
2013-06-04 15:28:57 +0800

    create project

---
+++ src/com/issc/ui/ActivityAIO.java
@@ -0,0 +1,13 @@
+// vim: et sw=4 sts=4 tabstop=4
+package com.issc.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityAIO extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aio);
+    }
+}

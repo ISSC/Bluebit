c8c283e03d49ad8411a55754037908cd6d026eb8
Julian_Chu <julian_chu@issc-tech.com>
2013-06-05 09:50:24 +0800

    Add Bluebit class
    This is the class to store global constants

---
+++ src/com/issc/Bluebit.java
@@ -0,0 +1,16 @@
+// vim: et sw=4 sts=4 tabstop=4
+package com.issc;
+
+/**
+ * This is the class to store global constant.
+ */
+
+public final class Bluebit {
+
+    public final static String TAG = "Bluebit";
+
+    private Bluebit() {
+        // Hide constructor since you should
+        // never instantiate me.
+    }
+}

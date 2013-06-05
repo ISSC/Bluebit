344b4d4eb6adc44818711bf8766db5e4b11f7ba8
Julian_Chu <julian_chu@issc-tech.com>
2013-06-05 11:43:31 +0800

    ui: Add button to launch Activity
    We are still building scaffolding, get a simple button
    to launch activity.

--- src/com/issc/ui/ActivityAIO.java
+++ src/com/issc/ui/ActivityAIO.java
@@ -1,13 +1,22 @@
 // vim: et sw=4 sts=4 tabstop=4
 package com.issc.ui;
 
+import com.issc.util.Log;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityAIO extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aio);
+    }
+
+    public void onClickBtn(View v) {
+        Intent i = new Intent(this, ActivityDevicesList.class);
+        startActivity(i);
     }
 }

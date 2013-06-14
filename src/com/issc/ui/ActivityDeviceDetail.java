d72bbfa0e3950ef83d4d722ca752e71c825ced7d
Julian_Chu <julian_chu@issc-tech.com>
2013-06-14 17:41:14 +0800

    ui: Display Details in ListView
    we might append arbitrary data into the screen, use ListView
    would be better

--- src/com/issc/ui/ActivityDeviceDetail.java
+++ src/com/issc/ui/ActivityDeviceDetail.java
@@ -6,28 +6,34 @@ import com.issc.R;
 import com.issc.util.Log;
 import com.issc.util.Util;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.app.Activity;
+import android.app.ListActivity;
 import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.ParcelUuid;
 import android.view.View;
+import android.widget.BaseAdapter;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
-public class ActivityDeviceDetail extends Activity {
-    TextView mName;
-    TextView mAddr;
-    TextView mDeviceClass;
+public class ActivityDeviceDetail extends ListActivity {
+
+    private final static String sKey = "key";
+    private final static String sVal = "value";
+
+    private ArrayList<Map<String, Object>> mEntries;
+    private BaseAdapter mAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_detail);
-
-        mName  = (TextView) findViewById(R.id.detail_name);
-        mAddr  = (TextView) findViewById(R.id.detail_addr);
-        mDeviceClass = (TextView) findViewById(R.id.detail_device_class);
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
@@ -35,13 +41,35 @@ public class ActivityDeviceDetail extends Activity {
             finish();
         }
 
+        initAdapter();
+
         BluetoothDevice bd = intent.getParcelableExtra(Bluebit.CHOSEN_DEVICE);
         init(bd);
     }
 
+    private void initAdapter() {
+        String[] from = {sKey, sVal};
+        int[] to = {R.id.row_title, R.id.row_description};
+
+        mEntries = new ArrayList<Map<String, Object>>();
+        mAdapter = new SimpleAdapter(
+                    this,
+                    mEntries,
+                    R.layout.row_detail,
+                    from,
+                    to
+                );
+
+        setListAdapter(mAdapter);
+    }
+
+
+    public void onClickBtnMore(View v) {
+    }
+
     private void init(BluetoothDevice device) {
-        mName.setText(device.getName());
-        mAddr.setText(device.getAddress());
+        append(getString(R.string.detail_name), device.getName());
+        append(getString(R.string.detail_addr), device.getAddress());
         setDeviceClass(device);
     }
 
@@ -49,15 +77,24 @@ public class ActivityDeviceDetail extends Activity {
         BluetoothClass clazz = device.getBluetoothClass();
         if (clazz == null) {
             Log.e("No Bluetooth Class");
-            mDeviceClass.setText("None");
+            append(getString(R.string.detail_device_class), "None");
         } else {
             int major = clazz.getMajorDeviceClass();
             int res = Util.getDeviceClassRes(major);
             if (res == -1) {
-                mDeviceClass.setText("Unknown");
+                append(getString(R.string.detail_device_class), "Unknown");
             } else {
-                mDeviceClass.setText(res);
+                append(getString(R.string.detail_device_class),
+                            getString(res));
             }
         }
+    }
+
+    private void append(String key, String value) {
+        Map<String, Object> entry = new HashMap<String, Object>();
+        entry.put(sKey, key);
+        entry.put(sVal, value);
+        mEntries.add(entry);
+        mAdapter.notifyDataSetChanged();
     }
 }

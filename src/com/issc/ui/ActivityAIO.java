86330329cb0c9546242bd4e15c1892a19c5b37ad
Julian_Chu <julian_chu@issc-tech.com>
2013-06-19 09:57:36 +0800

    it just work

--- src/com/issc/ui/ActivityAIO.java
+++ src/com/issc/ui/ActivityAIO.java
@@ -7,6 +7,8 @@ import com.issc.R;
 import com.issc.util.Log;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.UUID;
 import java.util.List;
 
 import android.app.Activity;
@@ -24,6 +26,7 @@ import android.widget.ToggleButton;
 import com.samsung.android.sdk.bt.gatt.BluetoothGatt;
 import com.samsung.android.sdk.bt.gatt.BluetoothGattAdapter;
 import com.samsung.android.sdk.bt.gatt.BluetoothGattCallback;
+import com.samsung.android.sdk.bt.gatt.BluetoothGattCharacteristic;
 import com.samsung.android.sdk.bt.gatt.BluetoothGattService;
 
 public class ActivityAIO extends Activity implements
@@ -81,15 +84,41 @@ public class ActivityAIO extends Activity implements
         if (id == CONNECTION_DIALOG) {
             mConnectionDialog = new ProgressDialog(this);
             mConnectionDialog.setMessage(this.getString(R.string.connecting));
-            mConnectionDialog.setCancelable(false);
+            mConnectionDialog.setCancelable(true);
             return mConnectionDialog;
         }
         return null;
     }
 
+    private final static UUID SERVICE_AUTOMATION_IO = UUID.fromString("00001815-0000-1000-8000-00805f9b34fb");
+    private final static UUID CHAR_DI = UUID.fromString("00002a56-0000-1000-8000-00805f9b34fb");
+    private final static UUID CHAR_DO = UUID.fromString("00002a57-0000-1000-8000-00805f9b34fb");
+
     public void onToggleClicked(View v) {
         ToggleButton toggle = (ToggleButton)v;
         Log.d("is checked:" + toggle.isChecked());
+
+        BluetoothGattService srv = mGatt.getService(mDevice, SERVICE_AUTOMATION_IO);
+        if (srv == null) {
+            Log.d("Get Service failed");
+        } else {
+            dumpService(srv);
+        }
+    }
+
+    private void dumpService(BluetoothGattService srv) {
+        BluetoothGattCharacteristic ch = srv.getCharacteristic(CHAR_DO);
+        if (ch == null) {
+            Log.d("get char failed");
+        } else {
+            byte ctrl = (byte)(int)(Math.random() * 2);
+            byte[] value = {(byte)0xfc, (byte)0xfe};
+            value[0] += ctrl;
+            value[1] += ctrl;
+            ch.setValue(value);
+            boolean r = mGatt.writeCharacteristic(ch);
+            Log.d(String.format("ctrl:%1x,%02x %02x, %b", ctrl, value[0], value[1], r));
+        }
     }
 
     @Override
@@ -142,25 +171,38 @@ public class ActivityAIO extends Activity implements
         }
     }
 
+    private void onConnected() {
+        List<BluetoothGattService> list = mGatt.getServices(mDevice);
+        if ((list == null) || (list.size() == 0)) {
+            Log.d("no services, do discovery");
+            mGatt.discoverServices(mDevice);
+        } else {
+            Log.d("services found, dismiss dialog");
+            updateView(DISMISS_CONNECTION_DIALOG, null);
+            mServices.addAll(mGatt.getServices(mDevice));
+        }
+    }
+
     class GattCallback extends BluetoothGattCallback {
         @Override
         public void onAppRegistered(int status) {
             updateView(SHOW_CONNECTION_DIALOG, null);
-            Log.d("callback registered, connecting to device");
-            mGatt.connect(mDevice, true);
+            Log.d("callback registered");
+            int conn = mGatt.getConnectionState(mDevice);
+            if (conn == BluetoothProfile.STATE_DISCONNECTED) {
+                Log.d("disconnected, connecting to device");
+                mGatt.connect(mDevice, true);
+            } else {
+                Log.d("already connected");
+                onConnected();
+            }
         }
 
         @Override
         public void onConnectionStateChange(BluetoothDevice device,
                 int status, int newState) {
             if (newState ==  BluetoothProfile.STATE_CONNECTED) {
-                List<BluetoothGattService> list = mGatt.getServices(device);
-                if ((list == null) || (list.size() == 0)) {
-                    mGatt.discoverServices(mDevice);
-                } else {
-                    updateView(DISMISS_CONNECTION_DIALOG, null);
-                    mServices.addAll(mGatt.getServices(device));
-                }
+                onConnected();
             } else if (newState ==  BluetoothProfile.STATE_DISCONNECTED) {
                 updateView(SHOW_CONNECTION_DIALOG, null);
             }
@@ -171,6 +213,15 @@ public class ActivityAIO extends Activity implements
             updateView(DISMISS_CONNECTION_DIALOG, null);
             mServices.addAll(mGatt.getServices(device));
             Log.d("found services:" + mServices.size());
+        }
+
+        public void onCharacteristicRead(BluetoothGattCharacteristic charac, int status) {
+            Log.d("read char, uuid=" + charac.getUuid().toString());
+            byte[] value = charac.getValue();
+            Log.d("get value, byte length:" + value.length);
+            for (int i = 0; i < value.length; i++) {
+                Log.d("[" + i + "]" + Byte.toString(value[i]));
+            }
         }
     }
 }

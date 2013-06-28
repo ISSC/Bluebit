696cf65e4eec9328a832d08a1018f71c0c725b0c
Julian_Chu <julian_chu@issc-tech.com>
2013-06-28 09:57:26 +0800

    workaround to connect to Weight Scale

--- src/com/issc/ui/ActivityWeight.java
+++ src/com/issc/ui/ActivityWeight.java
@@ -9,12 +9,14 @@ import com.issc.R;
 import com.issc.util.Log;
 import com.issc.util.Util;
 
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -41,11 +43,17 @@ import android.widget.Toast;
 import com.samsung.android.sdk.bt.gatt.BluetoothGatt;
 import com.samsung.android.sdk.bt.gatt.BluetoothGattAdapter;
 import com.samsung.android.sdk.bt.gatt.BluetoothGattCallback;
+import com.samsung.android.sdk.bt.gatt.BluetoothGattService;
 
 public class ActivityWeight extends Activity {
 
     private BluetoothGatt mGatt;
     private GattProxy.Listener mListener;
+
+    private final static UUID mAdvData = Util.uuidFromStr("FFF0");
+    private final static String ADDR = "78:C5:E5:6E:19:F2";
+
+    private BluetoothDevice mDevice;
 
     private TextView mKg;
     private TextView mLb;
@@ -80,7 +88,6 @@ public class ActivityWeight extends Activity {
     protected void onDestroy() {
         super.onDestroy();
         GattProxy proxy = GattProxy.get(ActivityWeight.this);
-        proxy.releaseGatt();
     }
 
     @Override
@@ -94,6 +101,7 @@ public class ActivityWeight extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
+        stopScanningTarget();
         GattProxy proxy = GattProxy.get(this);
         proxy.rmListener(mListener);
     }
@@ -132,23 +140,72 @@ public class ActivityWeight extends Activity {
         mName.setText(newName);
     }
 
-    private void startDiscovery() {
-        Log.d("Scanning Devices");
+    private void connect() {
+        mGatt.connect(mDevice, false);
+    }
+
+    private void scanTarget() {
+        mDevice = null;
         if (mGatt != null) {
-            /* connected device will not be ignored when scanning */
+            Log.d("Scanning Target");
+            // does not work yet.
+            //UUID[] uuids = new UUID[1];
+            //uuids[0] = mAdvData;
+            //mGatt.startScan(uuids);
             mGatt.startScan();
         } else {
             Log.e("No Gatt instance");
         }
     }
 
-    private void stopDiscovery() {
+    private void stopScanningTarget() {
         Log.d("Stop scanning");
         if (mGatt != null) {
             mGatt.stopScan();
         } else {
             Log.e("No Gatt instance");
         }
+    }
+
+    private void onConnected() {
+        List<BluetoothGattService> list = mGatt.getServices(mDevice);
+        if ((list == null) || (list.size() == 0)) {
+            Log.d("no services, do discovery");
+            mGatt.discoverServices(mDevice);
+        } else {
+            onDiscovered();
+        }
+    }
+
+    private void onDiscovered() {
+        Log.d("Discovered services!!");
+        List<BluetoothGattService> list = mGatt.getServices(mDevice);
+        Util.dumpServices(list);
+    }
+
+    private void onDisconnected() {
+        scanTarget();
+    }
+
+    private void onFoundTarget(BluetoothDevice dev, byte[] records) {
+        if (mDevice == null) {
+            mDevice = dev;
+            stopScanningTarget();
+            connect();
+        }
+    }
+
+    private boolean isTheTarget(BluetoothDevice device, byte[] records) {
+        String name = "Electronic Scales";
+        final int idx = 19; // the char 'E', we got it by dumping Scan response
+        ByteBuffer buf = ByteBuffer.allocate(name.length());
+        buf.put(records, idx, buf.limit());
+        String target = new String(buf.array());
+
+        if (name.equals(target)) {
+            return true;
+        }
+        return false;
     }
 
     class GattListener extends GattProxy.ListenerHelper {
@@ -160,10 +217,35 @@ public class ActivityWeight extends Activity {
         public void onRetrievedGatt(BluetoothGatt gatt) {
             Log.d(String.format("onRetrievedGatt"));
             mGatt = gatt;
+            scanTarget();
         }
 
         @Override
         public void onScanResult(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            Log.d("Scanned:" + device.getAddress());
+            if (isTheTarget(device, scanRecord)) {
+                onFoundTarget(device, scanRecord);
+            }
+        }
+
+        @Override
+        public void onConnectionStateChange(BluetoothDevice device,
+                int status, int newState) {
+            if (!mDevice.getAddress().equals(device.getAddress())) {
+                // not the device I care about
+                return;
+            }
+            if (newState ==  BluetoothProfile.STATE_CONNECTED) {
+                Log.d("connected to device");
+                onConnected();
+            } else if (newState ==  BluetoothProfile.STATE_DISCONNECTED) {
+                onDisconnected();
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothDevice device, int status) {
+            onDiscovered();
         }
     }
 }

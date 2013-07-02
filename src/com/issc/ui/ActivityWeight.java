b0aceecdb896a5edcd5c91eddffcd00dd28bae3d
Julian_Chu <julian_chu@issc-tech.com>
2013-07-02 08:55:42 +0800

    try read name but not work

--- src/com/issc/ui/ActivityWeight.java
+++ src/com/issc/ui/ActivityWeight.java
@@ -238,6 +238,7 @@ public class ActivityWeight extends Activity implements
         mQueue.add(t);
 
         enableAirPatch();
+        readName();
     }
 
     private boolean isProprietary() {
@@ -253,6 +254,34 @@ public class ActivityWeight extends Activity implements
         byte[] enable = {(byte)0x03};
         GattTransaction t = new GattTransaction(mAirPatch, enable);
         mQueue.add(t);
+    }
+
+    private void readName() {
+        if (!isProprietary()) {
+            return;
+        }
+
+        mGatt.setCharacteristicNotification(mAirPatch, true);
+        BluetoothGattDescriptor ccc =
+            mAirPatch.getDescriptor(Bluebit.DES_CLIENT_CHR_CONFIG);
+
+        GattTransaction t = new GattTransaction(ccc,
+                BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+        Log.d("enable notification for proprietary");
+        mQueue.add(t);
+
+        final int max = 16;
+        ByteBuffer e2prom = ByteBuffer.allocate(
+                Bluebit.CMD_READ_E2PROM.length +
+                Bluebit.ADDR_E2PROM_NAME.length +
+                1 +  // 1 byte for length
+                max);
+        e2prom.put(Bluebit.CMD_READ_E2PROM);   // read e2prom
+        e2prom.put(Bluebit.ADDR_E2PROM_NAME);  // 0x000b -> addr of name on e2prom
+        e2prom.put((byte)max);
+        GattTransaction t1 = new GattTransaction(mAirPatch, e2prom.array());
+        Log.d("read name by proprietary");
+        mQueue.add(t1);
     }
 
     private void writeName(CharSequence name) {
@@ -466,10 +495,21 @@ public class ActivityWeight extends Activity implements
         @Override
         public void onCharacteristicChanged(BluetoothGattCharacteristic chrc) {
             Log.d("on chr changed");
-            final int index = 4; // got this index from Frontline
-            byte[] data = chrc.getValue();
-            int value = ((0xFF & data[index]) << 8) + (0xFF & data[index + 1]);
-            updateValue(value);
+            if (chrc.getUuid().equals(mFFF4.getUuid())) {
+                Log.d("Got Weight value, update view");
+                final int index = 4; // got this index from Frontline
+                byte[] data = chrc.getValue();
+                int value = ((0xFF & data[index]) << 8) + (0xFF & data[index + 1]);
+                updateValue(value);
+            } else if (chrc.getUuid().equals(mAirPatch.getUuid())){
+                Log.d("Got Value update from AirPatch");
+                byte[] data = chrc.getValue();
+                for (int i = 0; i < data.length; i++) {
+                    Log.d(String.format("[%d]: 0x%02x", i, data[i]));
+                }
+            } else {
+                Log.d("Unknown chr:" + chrc.getUuid().toString());
+            }
         }
 
         @Override

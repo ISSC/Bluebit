858d79331516729b0cc3dccf90f5702287a89076
Julian_Chu <julian_chu@issc-tech.com>
2013-10-17 13:20:02 +0800

    widget: add LoadingFragment
    a useful widget for displaying Busy-icon.

---
+++ src/com/issc/widget/LoadingFragment.java
@@ -0,0 +1,40 @@
+// vim: et sw=4 sts=4 tabstop=4
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.issc.widget;
+
+import com.issc.R;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoadingFragment extends DialogFragment {
+
+    public LoadingFragment() {
+        super();
+        setStyle(DialogFragment.STYLE_NO_FRAME, 0);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.progressdialog, container, false);
+        return v;
+    }
+}
